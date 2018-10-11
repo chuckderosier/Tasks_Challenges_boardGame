1d248a2fb36b669d10fc30aafc847589fe7bee00
chuckderosier <chuckderosier2018@gmail.com>
2018-10-11 10:11:40 -0400

    creating grid pt 2

--- jQuery.js
+++ jQuery.js
@@ -1,206 +1,212 @@
 $(function(){ // run jQuery first
     console.log("jQuery is working")
 })
-function createDivs () {  // create spaces with div and class
+  // create spaces with div and class
+let newDivs = []
+function createDivs () {
     for (let i = 0; i < 36; i++) {
         let newDivs = document.createElement(`div`)
+        $(newDivs).addClass(`s` + [i])
     }
 }
-createDivs() // call div maker
-spaces = { // spaces object an value
-    .0: {
-        dice: d0,
-        gainDice: func,
-        text: ``
-    },
-    .1: {
-        dice: d0,
-        gainDice: func,
-        text: ``
-    },
-    .2: {
-        dice: d0,
-        gainDice: func,
-        text: ``
-    },
-    .3: {
-        dice: d0,
-        gainDice: func,
-        text: ``
-    },
-    .4: {
-        dice: d0,
-        gainDice: func,
-        text: ``
-    },
-    .5: {
-        dice: d0,
-        gainDice: func,
-        text: ``
-    },
-    .6: {
-        dice: d0,
-        gainDice: func,
-        text: ``
-    },
-    .7: {
-        dice: d0,
-        gainDice: func,
-        text: ``
-    },
-    .8: {
-        dice: d0,
-        gainDice: func,
-        text: ``
-    },
-    .9: {
-        dice: d0,
-        gainDice: func,
-        text: ``
-    },
-    .10: {
-        dice: d0,
-        gainDice: func,
-        text: ``
-    },
-    .11: {
-        dice: d0,
-        gainDice: func,
-        text: ``
-    },
-    .12: {
-        dice: d0,
-        gainDice: func,
-        text: ``
-    },
-    .13: {
-        dice: d0,
-        gainDice: func,
-        text: ``
-    },
-    .14: {
-        dice: d0,
-        gainDice: func,
-        text: ``
-    },
-    .15: {
-        dice: d0,
-        gainDice: func,
-        text: ``
-    },
-    .16: {
-        dice: d0,
-        gainDice: func,
-        text: ``
-    },
-    .17: {
-        dice: d0,
-        gainDice: func,
-        text: ``
-    },
-    .18: {
-        dice: d0,
-        gainDice: func,
-        text: ``
-    },
-    .19: {
-        dice: d0,
-        gainDice: func,
-        text: ``
-    },
-    .20: {
-        dice: d0,
-        gainDice: func,
-        text: ``
-    },
-    .21: {
-        dice: d0,
-        gainDice: func,
-        text: ``
-    },
-    .22: {
-        dice: d0,
-        gainDice: func,
-        text: ``
-    },
-    .23: {
-        dice: d0,
-        gainDice: func,
-        text: ``
-    },
-    .24: {
-        dice: d0,
-        gainDice: func,
-        text: ``
-    },
-    .25: {
-        dice: d0,
-        gainDice: func,
-        text: ``
-    },
-    .26: {
-        dice: d0,
-        gainDice: func,
-        text: ``
-    },
-    .27: {
-        dice: d0,
-        gainDice: func,
-        text: ``
-    },
-    .28: {
-        dice: d0,
-        gainDice: func,
-        text: ``
-    },
-    .29: {
-        dice: d0,
-        gainDice: func,
-        text: ``
-    },
-    .28: {
-        dice: d0,
-        gainDice: func,
-        text: ``
-    },
-    .29: {
-        dice: d0,
-        gainDice: func,
-        text: ``
-    },
-    .30: {
-        dice: d0,
-        gainDice: func,
-        text: ``
-    },
-    .31: {
-        dice: d0,
-        gainDice: func,
-        text: ``
-    },
-    .32: {
-        dice: d0,
-        gainDice: func,
-        text: ``
-    },
-    .33: {
-        dice: d0,
-        gainDice: func,
-        text: ``
-    },
-    .34: {
-        dice: d0,
-        gainDice: func,
-        text: ``
-    },
-    .35: {
-        dice: d0,
-        gainDice: func,
-        text: ``
-    }
-}
+ // call div maker
+createDivs()
+ // spaces object an value
+// spaces = {
+//     .s0: {
+//         dice: d0,
+//         gainDice: func,
+//         text: ``
+//     },
+//     .s1: {
+//         dice: d0,
+//         gainDice: func,
+//         text: ``
+//     },
+//     .s2: {
+//         dice: d0,
+//         gainDice: func,
+//         text: ``
+//     },
+//     .s3: {
+//         dice: d0,
+//         gainDice: func,
+//         text: ``
+//     },
+//     .s4: {
+//         dice: d0,
+//         gainDice: func,
+//         text: ``
+//     },
+//     .s5: {
+//         dice: d0,
+//         gainDice: func,
+//         text: ``
+//     },
+//     .s6: {
+//         dice: d0,
+//         gainDice: func,
+//         text: ``
+//     },
+//     .s7: {
+//         dice: d0,
+//         gainDice: func,
+//         text: ``
+//     },
+//     .s8: {
+//         dice: d0,
+//         gainDice: func,
+//         text: ``
+//     },
+//     .s9: {
+//         dice: d0,
+//         gainDice: func,
+//         text: ``
+//     },
+//     .s10: {
+//         dice: d0,
+//         gainDice: func,
+//         text: ``
+//     },
+//     .s11: {
+//         dice: d0,
+//         gainDice: func,
+//         text: ``
+//     },
+//     .s12: {
+//         dice: d0,
+//         gainDice: func,
+//         text: ``
+//     },
+//     .s13: {
+//         dice: d0,
+//         gainDice: func,
+//         text: ``
+//     },
+//     .s14: {
+//         dice: d0,
+//         gainDice: func,
+//         text: ``
+//     },
+//     .s15: {
+//         dice: d0,
+//         gainDice: func,
+//         text: ``
+//     },
+//     .s16: {
+//         dice: d0,
+//         gainDice: func,
+//         text: ``
+//     },
+//     .s17: {
+//         dice: d0,
+//         gainDice: func,
+//         text: ``
+//     },
+//     .s18: {
+//         dice: d0,
+//         gainDice: func,
+//         text: ``
+//     },
+//     .s19: {
+//         dice: d0,
+//         gainDice: func,
+//         text: ``
+//     },
+//     .s20: {
+//         dice: d0,
+//         gainDice: func,
+//         text: ``
+//     },
+//     .s21: {
+//         dice: d0,
+//         gainDice: func,
+//         text: ``
+//     },
+//     .s22: {
+//         dice: d0,
+//         gainDice: func,
+//         text: ``
+//     },
+//     .s23: {
+//         dice: d0,
+//         gainDice: func,
+//         text: ``
+//     },
+//     .s24: {
+//         dice: d0,
+//         gainDice: func,
+//         text: ``
+//     },
+//     .s25: {
+//         dice: d0,
+//         gainDice: func,
+//         text: ``
+//     },
+//     .s26: {
+//         dice: d0,
+//         gainDice: func,
+//         text: ``
+//     },
+//     .s27: {
+//         dice: d0,
+//         gainDice: func,
+//         text: ``
+//     },
+//     .s28: {
+//         dice: d0,
+//         gainDice: func,
+//         text: ``
+//     },
+//     .s29: {
+//         dice: d0,
+//         gainDice: func,
+//         text: ``
+//     },
+//     .s28: {
+//         dice: d0,
+//         gainDice: func,
+//         text: ``
+//     },
+//     .s29: {
+//         dice: d0,
+//         gainDice: func,
+//         text: ``
+//     },
+//     .s30: {
+//         dice: d0,
+//         gainDice: func,
+//         text: ``
+//     },
+//     .s31: {
+//         dice: d0,
+//         gainDice: func,
+//         text: ``
+//     },
+//     .s32: {
+//         dice: d0,
+//         gainDice: func,
+//         text: ``
+//     },
+//     .s33: {
+//         dice: d0,
+//         gainDice: func,
+//         text: ``
+//     },
+//     .s34: {
+//         dice: d0,
+//         gainDice: func,
+//         text: ``
+//     },
+//     .s35: {
+//         dice: d0,
+//         gainDice: func,
+//         text: ``
+//     }
+// }
+ // constructor for players
 players = {
-    constructor () { // constructor for players
+    constructor () {
         this.name = ``;
         this.color = ``;
         this.shape = ``
