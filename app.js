aea64bad864198d534975d4b35dafb6f5d81da5b
chuckderosier <chuckderosier2018@gmail.com>
2018-10-12 12:12:18 -0400

    trying to move token

--- app.js
+++ app.js
@@ -12,11 +12,11 @@ let numDice
 let addPoints = 0
 let addDice = 0
 let rollNum = 0
+let moveDist = 0
 // create spaces with divs and classes
 let createDivs
 let createInDivs
-let moveDist = 0
-for (let i = 1; i < 24; i++) { // outer loop
+for (let i = 1; i < 25; i++) { // outer loop
     let createDivs = document.createElement(`div`)
     $(createDivs).addClass([i])
     $(createDivs).attr('id', `s` + [i])
@@ -206,11 +206,8 @@ $('.rules').click(function() { // rules button
     alert("—- The Goal — Get to the center of the board first. To get there, you have to overcome Challenges to earn Points and accomplish Tasks to earn Die.n/— Start — Player with the next birthday goes first. Each player begins at Start. Each Die you earn gives you one die to roll for use in completing Tasks or overcoming Challenges, explained below.n/—- Movement —- On your turn, roll to move around the board. You can move clockwise or counter-clockwise. You only play the spot on which you land./n—- Task Spaces —- blue spaces are 1 die, purple spaces are 2 dice, orange spaces are 3 dice —- When you land on a Task space the app will roll that number of dice as indicated the space. You roll a number all of your dice you’ve earned. If you roll lower than the total rolled for the Task you earn 1 Die when in outer loop or 2 Dice  when in inner loop to use on your next Task or Challenge. Obviously the more you Dice the harder it will be to accomplish Tasks but the easier it will be to overcome challenges. Tie rolls are successes./n—- Challenge Spaces —- red spaces are 1 die, pink spaces are 2 dice, brown spaces are 3 dice —- When you land on a Challenge space the app will roll that number of dice as indicated the space. You roll a number all of your dice you’ve earned. If you roll higher than the total rolled for the Challenge you earn 1 Point when in outer loop or 2 points when in inner loop. When you get 4 Points you may advance to the inner loop from the green space on the outer loop. When you get to 8 points you may enter the center space from the green space on the inner loop. Exact roll not necessary. You win.")
 }
 )
-// dice
+// player dice
 for (let k = player1Dice; k < player1Dice; k++) {
-    rollNum = Math.floor((Math.random() * 6) + 1)
-}
-for (let k = player2Dice; k < player2Dice; k++) {
     rollNum = Math.floor((Math.random() * 6) + 1)
 }
 // display players points and dice
@@ -219,23 +216,23 @@ $(".pointsPlayer1").text("Player 1 has " + player1Points + " points")
 $(".dicePlayer2").text("Player 1 has " + player2Dice + " dice")
 $(".pointsPlayer2").text("Player 1 has " + player2Points + " points")
 // players turn
-// if (player1Points < 10 || player2Points < 10) {
-//     $(`.windowRoll`).text(`Player 1 turn`)
-//     $(`.roll`).click(function() {
-//         let moveDist = Math.floor((Math.random() * 6) + 1)
-//     })
 
-//       });
-//     // if ($(moveDist + ))
-//     $(`.counter`).click(function() {
-//     })
+if (player1Points < 10 || player2Points < 10) {
+    $(`.windowRoll`).text(`Player 1 turn`)
+    $(`.roll`).click(function() {
+        moveDist = Math.floor((Math.random() * 6) + 1)
+        $(`.windowRoll`).text(`You rolled a ` + moveDist)
+        let spaceOn = $(`.play1`).parent().prop(`class`)    
+        spaceOn = parseInt(spaceOn)
+        // if ((moveDist + spaceOn) > 24) {
+        //     spaceOn = 24 - spaceOn
+        // }
+        spaceOn += moveDist
+        console.log(spaceOn)
+        $(`.play1`).append(spaceOn)
+    })
+}
 
-    // let spaceOn = $(`.play1`).parent().attr(`class`)
-    let spaceOn = $(`.play1`).parent().prop(`class`)
-    // let spaceOn =  Number($(`play1`).parent().attr(`id`))
-    // let spaceOn =  Number($(`play1`).parent().prop(`class`))
-    // parseInt(spaceOn)
-    console.log(spaceOn)
 // constructor for players
 // players = {
 //     constructor () {
