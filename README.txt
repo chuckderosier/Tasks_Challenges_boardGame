6b67a4f5ffa5d7a487e3aefa6329c278e406d883
chuckderosier <chuckderosier2018@gmail.com>
2018-10-11 20:44:15 -0400

    start create players and added README

---
+++ README.txt
@@ -0,0 +1,8 @@
+Board Game originally called Camp Drunk-A-Lot
+Players move around the board rolling a 6 sided die, when they land on a square they must either face a challenge of complete a task. On tasks they must roll all their dice and roll less than the total roll of the dice that is shown in the board space, and if they succeed they add another die to the ones they already have. On challenge spaces the player must beat the dice roll that is listed in the space, and on success they gain one point. When enough points are achieved the player may them move to the inner loop which is similar but with higher points and more dice. When the player gets enough points in the inner loop they may attempt to enter the winners space and face one last challenge.
+
+A Link to the Deployed App
+Trello : https://trello.com/b/KeKzryt3/project
+Libraries: jQuery.min (library uploaded, not linked)
+A Link to any Wirefames or Mockups (or inline photos / screenshots will work too!) Libraries, etc: jQuery
+Version 2: more styling, actual game board image, the extra spaces I didn’t include, personalize players
--- app.js
+++ app.js
@@ -9,6 +9,8 @@ let player1Points = 0
 let player2Dice = 1
 let player2Points = 0
 let computerDice
+let player1 = $(`.s0`).html(`<div class="play1"></div>`)
+let player2  = $(`.s0`).html(`<div class="play2"></div>`)
 for (let i = 0; i < 24; i++) { // outer loop
     let createDivs = document.createElement(`div`)
     $(createDivs).addClass(`s` + [i])
@@ -19,6 +21,7 @@ for (let j = 0; j < 12; j++) { // inner loop
     $(createInDivs).addClass(`is` + [j])
     $(`.inLoop`).append(createInDivs)
 }
+// roll to move
  // spaces pair values
 spaces = {
     s0: {
@@ -204,14 +207,15 @@ spaces = {
 }
 // nav bar items
 $('.rules').click(function() { // rules button
-    alert("—- The Goal — Get to the center of the board first and overcome the final challenge - die happily at the water and let the vultures pick your bones clean. To get there, you have to overcome Challenges to earn Points and accomplish Tasks to earn Die. — Start — Player with the next birthday goes first. Each player begins at Start. Each Die you earn gives you one die to roll for use in completing Tasks or overcoming Challenges, explained below. —- Movement —- On your turn, roll 1 die to move around the board. You can move clockwise or counter-clockwise on your turn, your choice. You only play the spot on which you land, i.e. if you roll a 4, move 4 spaces and play the space you’re on. Regardless of how many Dice you have, you still only roll 1 die to move around the board. —- Task Spaces —- blue spaces are 1 die, purple spaces are 2 dice, orange spaces are 3 dice —- When you land on a Task space the app will roll that number of dice as indicated the space. You roll a number all of your dice you’ve earned. If you roll lower than the total rolled for the Task you earn 1 Die when in outer loop or 2 Dice  when in inner loop to use on your next Task or Challenge. Obviously the more you Dice the harder it will be to accomplish Tasks – but the easier it will be to overcome challenges. Tie rolls are successes. —- Challenge Spaces —- red spaces are 1 die, pink spaces are 2 dice, brown spaces are 3 dice —- When you land on a Challenge space the app will roll that number of dice as indicated the space. You roll a number all of your dice you’ve earned. If you roll higher than the total rolled for the Challenge you earn 1 Point when in outer loop or 2 points when in inner loop. When you get 4 Points you may advance to the inner loop from the green space on the outer loop. When you get to 8 points you may enter the center space from the green space on the inner loop. Exact roll not necessary. You win.")
+    alert("—- The Goal — Get to the center of the board first. To get there, you have to overcome Challenges to earn Points and accomplish Tasks to earn Die.n/— Start — Player with the next birthday goes first. Each player begins at Start. Each Die you earn gives you one die to roll for use in completing Tasks or overcoming Challenges, explained below.n/—- Movement —- On your turn, roll to move around the board. You can move clockwise or counter-clockwise. You only play the spot on which you land./n—- Task Spaces —- blue spaces are 1 die, purple spaces are 2 dice, orange spaces are 3 dice —- When you land on a Task space the app will roll that number of dice as indicated the space. You roll a number all of your dice you’ve earned. If you roll lower than the total rolled for the Task you earn 1 Die when in outer loop or 2 Dice  when in inner loop to use on your next Task or Challenge. Obviously the more you Dice the harder it will be to accomplish Tasks but the easier it will be to overcome challenges. Tie rolls are successes./n—- Challenge Spaces —- red spaces are 1 die, pink spaces are 2 dice, brown spaces are 3 dice —- When you land on a Challenge space the app will roll that number of dice as indicated the space. You roll a number all of your dice you’ve earned. If you roll higher than the total rolled for the Challenge you earn 1 Point when in outer loop or 2 points when in inner loop. When you get 4 Points you may advance to the inner loop from the green space on the outer loop. When you get to 8 points you may enter the center space from the green space on the inner loop. Exact roll not necessary. You win.")
 }
 )
+// display players points and dice
 $(".dicePlayer1").text("Player 1 has " + player1Dice + " dice")
 $(".pointsPlayer1").text("Player 1 has " + player1Points + " points")
 $(".dicePlayer2").text("Player 1 has " + player2Dice + " dice")
 $(".pointsPlayer2").text("Player 1 has " + player2Points + " points")
- // constructor for players
+// constructor for players
 // players = {
 //     constructor () {
 //         this.name = ``;
