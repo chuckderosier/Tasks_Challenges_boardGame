c01cbc38bcdf3c51b643a9a9d149b2886345c1bb
chuckderosier <chuckderosier2018@gmail.com>
2018-10-12 14:34:02 -0400

    both directions are working

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-$(function(){ // run jQuery first
+$(function () { // run jQuery first
     console.log("jQuery is working")
 })
 
@@ -28,8 +28,8 @@ for (let i = 1; i < 25; i++) { // outer loop
 //     $(`.inLoop`).append(createInDivs)
 // }
 let player1 = $(`#s1`).append(`<div class="play1"></div>`)
-let player2  = $(`#s1`).append(`<div class="play2"></div>`)
- // spaces pair values
+let player2 = $(`#s1`).append(`<div class="play2"></div>`)
+// spaces pair values
 spaces = {
     s1: {
         type: `free`,
@@ -202,7 +202,7 @@ spaces = {
 }
 //     
 // nav bar items
-$('.rules').click(function() { // rules button
+$('.rules').click(function () { // rules button
     alert("—- The Goal — Get to the center of the board first. To get there, you have to overcome Challenges to earn Points and accomplish Tasks to earn Die.n/— Start — Player with the next birthday goes first. Each player begins at Start. Each Die you earn gives you one die to roll for use in completing Tasks or overcoming Challenges, explained below.n/—- Movement —- On your turn, roll to move around the board. You can move clockwise or counter-clockwise. You only play the spot on which you land./n—- Task Spaces —- blue spaces are 1 die, purple spaces are 2 dice, orange spaces are 3 dice —- When you land on a Task space the app will roll that number of dice as indicated the space. You roll a number all of your dice you’ve earned. If you roll lower than the total rolled for the Task you earn 1 Die when in outer loop or 2 Dice  when in inner loop to use on your next Task or Challenge. Obviously the more you Dice the harder it will be to accomplish Tasks but the easier it will be to overcome challenges. Tie rolls are successes./n—- Challenge Spaces —- red spaces are 1 die, pink spaces are 2 dice, brown spaces are 3 dice —- When you land on a Challenge space the app will roll that number of dice as indicated the space. You roll a number all of your dice you’ve earned. If you roll higher than the total rolled for the Challenge you earn 1 Point when in outer loop or 2 points when in inner loop. When you get 4 Points you may advance to the inner loop from the green space on the outer loop. When you get to 8 points you may enter the center space from the green space on the inner loop. Exact roll not necessary. You win.")
 }
 )
@@ -219,30 +219,36 @@ $(".pointsPlayer2").text("Player 1 has " + player2Points + " points")
 
 if (player1Points < 10 || player2Points < 10) {
     $(`.windowRoll`).text(`Player 1 turn`)
-    $(`.roll`).click(function() {
+    $(`.roll`).one(`click`, function () {
         moveDist = Math.floor((Math.random() * 6) + 1)
-
         $(`.windowRoll`).text(`You rolled a ` + moveDist)
-        let spaceOn = $(`.play1`).parent().prop(`class`)    
+        let spaceOn = $(`.play1`).parent().prop(`class`)
+        let youClicked = 0
         spaceOn = parseInt(spaceOn)
-        // if ((moveDist + spaceOn) > 24) {
-        //     spaceOn = 24 - spaceOn
-        // }
-        spaceOn += moveDist
-        $(`.counter`).click(function() {
-            if (spaceOn > 24) {
-                spaceOn = (24 - spaceOn)*-1
-                $(`.play1`).appendTo(`.` + spaceOn)
-                console.log(spaceOn)
-            } else {
-                $(`.play1`).appendTo(`.` + spaceOn)
-            }
-        })
-        // $(`.clock`).click(function() {
-        //     spaceOn -= spaceOn*2
-        //     $(`.play1`).appendTo(`.` + spaceOn)
-        //     console.log(spaceOn)
-        // })
+        if (youClicked !== 0) {
+            $(`.counter`).one(`click`, function () {
+                youClicked += 1
+                spaceOn += moveDist
+                if (spaceOn > 24) {
+                    spaceOn = (24 - spaceOn) * -1
+                    $(`.play1`).appendTo(`.` + spaceOn)
+                } else {
+                    $(`.play1`).appendTo(`.` + spaceOn)
+                }
+            })
+        }
+        if (youClicked !== 0) {
+            $(`.clock`).one(`click`, function () {
+                youClicked += 1
+                spaceOn -= moveDist
+                if (spaceOn > 0) {
+                    $(`.play1`).appendTo(`.` + spaceOn)
+                } else {
+                    spaceOn = (24 + spaceOn)
+                    $(`.play1`).appendTo(`.` + spaceOn)
+                }
+            })
+        }
     })
 }
 
